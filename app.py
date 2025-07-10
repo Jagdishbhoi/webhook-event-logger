fcc472e0d0a5a61132a7d25ce4a39e373fac9f78
Jagdish <jagdishbhoi251@gmail.com>
2025-07-11 00:48:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import os
 
 app = Flask(__name__)
 
-# MongoDB URI - Replace with your real credentials
-app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/webhookDB"
+
+app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/webhookDB?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 @app.route("/")
